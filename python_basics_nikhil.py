a613573c6d9af22659c5ba59fb51e30a78e9c46a
BenQ <39688242+BenQ12345@users.noreply.github.com>
2026-04-06 15:43:12 +0530

    Add files via upload

---
+++ python_basics_nikhil.py
@@ -0,0 +1,22 @@
+
+
+First_name = "Nikhil"
+age = 25
+height = 5.8
+is_student = True
+print(f"{First_name} {age} {height} {is_student}")
+
+print(f"Name: {First_name} | Age:{age} | Height: {height} | Student: {is_student}")
+
+# Age in months
+Age_months = age * 12
+print("Age in months:", Age_months)
+
+Age_in_days = age * 365
+print("Age in days:", Age_in_days)
+
+remainder = age % 7
+print("Remainder:", remainder)
+
+Age_power = age ** 2
+print("Age_power:", Age_power)
